899ae34e44f3408a2d6e7291366926a8dfde3cfb
unknown <diegofbisquerra@gmail.com>
2022-09-20 11:31:10 -0300

    Homework Done

--- FT-M2-master/06-React-Intro/homework/src/components/Cards.jsx
+++ FT-M2-master/06-React-Intro/homework/src/components/Cards.jsx
@@ -1,7 +1,25 @@
 import React from 'react';
+import Card from './Card';
 
 export default function Cards(props) {
   // acá va tu código
   // tip, podés usar un map
-  return <div>Cards Component</div>
-};
+//   return <div>Cards Component</div>
+// };
+
+const { cities } = props;
+  return (
+    <div className="cards">
+      {cities.map((card, index) => (
+        <Card
+        key={card.id}
+        max={card.main.temp_max}
+        min={card.main.temp_min}
+        name={card.name}
+        img={card.weather[0].icon}
+        onClose={() => alert(card.name)}
+      />
+      ))}
+    </div>
+  );
+}
